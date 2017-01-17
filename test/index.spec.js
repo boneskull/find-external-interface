eb01c60d09e12216f781e5129c85f4f803a2c859
Christopher Hiller <boneskull@boneskull.com>
2017-01-16 22:35:29 -0800

    fix object reference problem in tests; add another case

--- test/index.spec.js
+++ test/index.spec.js
@@ -5,6 +5,7 @@ var os = require('os');
 var findExternalInterface = require('../index').findExternalInterface;
 var sinon = require('sinon');
 var allInterfaces = require('./interfaces.json');
+var cloneDeep = require('lodash.clonedeep');
 
 describe('findExternalInterface()', function () {
   var sandbox;
@@ -15,7 +16,7 @@ describe('findExternalInterface()', function () {
 
   describe('when an external interface is not present', function () {
     beforeEach(function () {
-      var interfaces = Object.assign({}, allInterfaces);
+      var interfaces = cloneDeep(allInterfaces);
       delete interfaces.en13;
       sandbox.stub(os, 'networkInterfaces')
         .returns(interfaces);
@@ -39,7 +40,7 @@ describe('findExternalInterface()', function () {
     });
   });
 
-  describe('when "IPv6" option is "true"', function () {
+  describe('when "IPv6" option is truthy', function () {
     beforeEach(function () {
       sandbox.stub(os, 'networkInterfaces')
         .returns(allInterfaces);
@@ -70,7 +71,7 @@ describe('findExternalInterface()', function () {
 
     describe('and the interface is internal', function () {
       beforeEach(function () {
-        var interfaces = Object.assign({}, allInterfaces);
+        var interfaces = cloneDeep(allInterfaces);
         interfaces.en14 = interfaces.en13;
         interfaces.en14[0].internal = true;
         sandbox.stub(os, 'networkInterfaces')
@@ -81,7 +82,19 @@ describe('findExternalInterface()', function () {
         expect(findExternalInterface({name: 'en14'})).to.be.null;
       });
     });
+  });
 
+  describe('when "name" option is an empty string', function () {
+    beforeEach(function () {
+      sandbox.stub(os, 'networkInterfaces')
+        .returns(allInterfaces);
+    });
+
+    it('should return the first external IPv4 interface', function () {
+      expect(findExternalInterface({name: ''}))
+        .to
+        .equal('en13');
+    });
   });
 
   afterEach(function () {
